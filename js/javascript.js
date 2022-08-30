aef42781c02f174b1ae4e93f47c2180838b56845
PietroDanieli <1pie7d@gmail.com>
2022-08-30 14:37:29 +0200

    complete cicle while for shopping-list

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+console.log("miao");
+
+const theList = [
+  "Sgabello",
+  "Formaggio",
+  "Borraccia blu",
+  "Cazzo di gomma",
+  "eeeeeeh.. Cannuccia!",
+];
+
+let i = 0;
+while (i < theList.length) {
+  document.getElementById(
+    "myList"
+  ).innerHTML += `<li class="mb-2">${theList[i]}</li>`;
+  i++;
+}
+/*
+for (i = 0; i < theList.length; i++) {
+  document.getElementById(
+    "myList"
+  ).innerHTML += `<li class="mb-2">${theList[i]}</li>`;
+}
+*/
